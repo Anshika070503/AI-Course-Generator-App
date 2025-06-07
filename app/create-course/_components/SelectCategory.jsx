992f7659de370c8120a8d24b1853b2b5c9585b08
Anshika070503 <anshikasinghal161@gmail.com>
2025-06-07 12:06:08 +0530

    Some issues fixed

--- app/create-course/_components/SelectCategory.jsx
+++ app/create-course/_components/SelectCategory.jsx
@@ -1,34 +1,37 @@
-import CategoryList from '@/app/_components/_shared/CategoryList'
+import CategoryList from '@/app/_components/_shared/CategoryList';
 import { UserInputContext } from '@/app/_context/UserInputContext';
-import Image from 'next/image'
-import React, { useContext } from 'react'
+import Image from 'next/image';
+import React, { useContext, useCallback } from 'react';
 
 function SelectCategory() {
-  const{userCourseInput,setUserCourseInput} = useContext(UserInputContext);
-  const handleCategoryChange=(category)=>{
-    setUserCourseInput(prev=>({
+  const { userCourseInput, setUserCourseInput } = useContext(UserInputContext);
+
+  const handleCategoryChange = useCallback((category) => {
+    setUserCourseInput(prev => ({
       ...prev,
-      category:category
-    }))
-  }
+      category: category
+    }));
+  }, [setUserCourseInput]);
+
   return (
     <div className='px-10 md:px-20'>
-       <h2 className='my-5'>Select the Course Category</h2>
-   
-     
-    <div className='grid grid-cols-3 gap-10 '>
-    
-      {CategoryList.map((item,index)=>(
-        <div className={`flex flex-col p-5 border items-center rounded-xl hover:border-primary hover:bg-blue-50 cursor-pointer 
-          ${userCourseInput?.category==item.name && 'border-primary bg-blue-50'}`}
-        onClick={()=>handleCategoryChange(item.name)}
-        >
-            <Image src={item.icon} width={50} height={45}/>
+      <h2 className='my-5'>Select the Course Category</h2>
+
+      <div className='grid grid-cols-3 gap-10'>
+        {CategoryList.map((item, index) => (
+          <div
+            key={item.name} // Prefer using unique id or name over index
+            className={`flex flex-col p-5 border items-center rounded-xl hover:border-primary hover:bg-blue-50 cursor-pointer 
+              ${userCourseInput?.category === item.name && 'border-primary bg-blue-50'}`}
+            onClick={() => handleCategoryChange(item.name)}
+          >
+            <Image src={item.icon} width={50} height={45} alt={item.name} />
             <h2>{item.name}</h2>
-        </div>
-      ))}
+          </div>
+        ))}
+      </div>
     </div>
-     </div>
   );
 }
-export default SelectCategory
+
+export default SelectCategory;
